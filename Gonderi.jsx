65f917d8e4172f1a8c09fa39bed9253336efa1f1
Selen Akın <127742376+selenakinn@users.noreply.github.com>
2026-02-22 23:30:08 +0300

    Add Gonderi component for post display

---
+++ Gonderi.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Yorumlar from '../Yorumlar/Yorumlar';
+import BegenBolumu from './BegenBolumu';
+import GonderiBasligi from './GonderiBasligi';
+
+const Gonderi = (props) => {
+  const { gonderi, gonderiyiBegen } = props;
+
+  return (
+    <div className="post-border">
+      <GonderiBasligi
+        username={gonderi.username}
+        thumbnailUrl={gonderi.thumbnailUrl}
+      />
+      <div className="post-image-wrapper">
+        <img
+          alt="post thumbnail"
+          className="post-image"
+          src={gonderi.imageUrl}
+        />
+      </div>
+      <BegenBolumu
+        gonderiyiBegen={() => gonderiyiBegen(gonderi.id)}
+        begeniSayisi={gonderi.likes}
+      />
+      <Yorumlar yorumlar={gonderi.comments} />
+    </div>
+  );
+};
+
+export default Gonderi;
